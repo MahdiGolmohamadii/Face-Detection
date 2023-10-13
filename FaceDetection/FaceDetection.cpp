2d2a189142b32b75ee3fcb3e1370e225409b85b0
MahdiGolmohamadii <mmahdigolmohamadi@gmail.com>
2023-10-13 10:53:44 +0330

    Add project files.

---
+++ FaceDetection/FaceDetection.cpp
@@ -0,0 +1,15 @@
+// FaceDetection.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
